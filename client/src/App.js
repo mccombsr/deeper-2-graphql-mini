1d376a9244e287149e84e0564c2cdd0b6506b6f4
Jeremy R <jeremy@devmounta.in>
2018-10-10 10:06:15 -0600

    step12

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Links from './Links';
+import AddLink from 'AddLink';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AddLink />
         <Links />
       </div>
     );
