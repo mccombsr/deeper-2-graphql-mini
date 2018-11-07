2b2f7e1c1adeb8730afc8bc3611f1f37bef6360a
Ryan McCombs <mccombs008@yahoo.com>
2018-11-07 11:05:57 -0700

    step 4 completed

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const typeDefs = `
         url: String!
     }
 `
+//This is just some dummy data. In a real app we'd use a database instead.
+let articleLinks = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'A resources to learn graphql. Check out the advanced sections for some more in-depth tutorials.'
+}, {
+    id: 'link-1',
+    url: 'news.ycombinator.com',
+    description: 'Hacker news is like reddit that doesn\'t suck.  Focused on tech.  Great place to improvey our chameleon skills.'
+}, {
+    id: 'link-2',
+    url: 'https://www.graphqlhub.com/',
+    description: 'Some practice APIs to play around with queries'
+}]
+let idCount = articleLinks.length
+
 //This object needs to match the structure of our typeDefinition Queries and Mutations
 //All values should be functions and what they return is like doing a res.send...almost.
 const resolvers = {
